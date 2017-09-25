67f560e2b47432dc282b2d5e665143811673835f
Natan Ximenes <natan.ximenes@concretesolutions.com.br>
2017-09-25 10:34:48 -0300

    Implements step 1 with the creation of a Java Library to tell jokes

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,46 +2,26 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import br.com.natanximenes.Comedian;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button jokeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        jokeButton = findViewById(R.id.joke_button);
+        jokeButton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onClick(View view) {
+        Toast.makeText(this, Comedian.tellJoke(), Toast.LENGTH_SHORT).show();
     }
-
-    public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-    }
-
-
 }
---
+++ FinalProject/jokeLib/src/main/java/br/com/natanximenes/Comedian.java
@@ -0,0 +1,8 @@
+package br.com.natanximenes;
+
+public class Comedian {
+
+    public static String tellJoke() {
+        return "How many androiders are necessary to change a lamp? Just one with Google Assistant!";
+    }
+}
