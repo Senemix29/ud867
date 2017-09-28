0ee9eefcc219e92c1e2f77a490dcb4d75cd0cdcb
Natan Ximenes <natan.ximenes@concretesolutions.com.br>
2017-09-28 19:55:03 -0300

    Rename jokelib and adds jokedisplayer dependency

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,12 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import br.com.natanximenes.Comedian;
+import br.com.natanximenes.jokeDisplayer.JokeDisplayerActivity;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(this, Comedian.tellJoke(), Toast.LENGTH_SHORT).show();
+        Intent jokeIntent = new Intent(this, JokeDisplayerActivity.class);
+        jokeIntent.putExtra(JokeDisplayerActivity.JOKE, Comedian.tellJoke());
+        startActivity(jokeIntent);
     }
 }
